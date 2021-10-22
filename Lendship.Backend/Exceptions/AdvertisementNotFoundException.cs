43e774f778beab2c159f54673f8de34f32d4fb2a
Bajmóczy Eszter Csilla <eszty.bajmoczy@gmail.com>
2021-10-22 18:50:23 +0200

    Create exception classes

---
+++ Lendship.Backend/Exceptions/AdvertisementNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lendship.Backend.Exceptions
+{
+    public class AdvertisementNotFoundException : Exception
+    {
+        public AdvertisementNotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ Lendship.Backend/Exceptions/UpdateNotAllowedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lendship.Backend.Exceptions
+{
+    public class UpdateNotAllowedException : Exception
+    {
+        public UpdateNotAllowedException(string message) : base(message)
+        {
+        }
+    }
+}
