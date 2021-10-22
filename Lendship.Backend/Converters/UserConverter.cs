d802890ace6ada6e61d5e8d7f3876db26c0ac7e8
Bajmóczy Eszter Csilla <eszty.bajmoczy@gmail.com>
2021-10-22 18:49:10 +0200

    Create Converters for: Advertisement, User, Availability

---
+++ Lendship.Backend/Converters/UserConverter.cs
@@ -0,0 +1,25 @@
+﻿using Lendship.Backend.Authentication;
+using Lendship.Backend.DTO;
+using Lendship.Backend.Interfaces.Converters;
+using Lendship.Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lendship.Backend.Converters
+{
+    public class UserConverter : IUserConverter
+    {
+        public UserDto ConvertToDto(ApplicationUser user, int evaluationAsLender, int evaluationAsAdvertiser)
+        {
+            return new UserDto
+            {
+                Id = new Guid(user.Id),
+                Name = user.UserName,
+                EvaluationAsAdvertiser = evaluationAsAdvertiser,
+                EvaluationAsLender = evaluationAsLender
+            };
+        }
+    }
+}
---
+++ Lendship.Backend/Interfaces/Converters/IUserConverter.cs
@@ -0,0 +1,14 @@
+﻿using Lendship.Backend.Authentication;
+using Lendship.Backend.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lendship.Backend.Interfaces.Converters
+{
+    public interface IUserConverter
+    {
+        UserDto ConvertToDto(ApplicationUser user, int evaluationAsLender, int evaluationAsAdvertiser);
+    }
+}
