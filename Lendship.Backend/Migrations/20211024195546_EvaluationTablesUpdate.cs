809f9190d8c9c8de4298650e7e2fa6c33fe34e30
Bajmóczy Eszter Csilla <36538209+EszterBajmoczy@users.noreply.github.com>
2021-10-24 21:56:28 +0200

    Migration: add Comment to evaluation tables

---
+++ Lendship.Backend/Migrations/20211024195546_EvaluationTablesUpdate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lendship.Backend.Migrations
+{
+    public partial class EvaluationTablesUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Comment",
+                table: "EvaluationLenders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Comment",
+                table: "EvaluationAdvertisers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comment",
+                table: "EvaluationLenders");
+
+            migrationBuilder.DropColumn(
+                name: "Comment",
+                table: "EvaluationAdvertisers");
+        }
+    }
+}
