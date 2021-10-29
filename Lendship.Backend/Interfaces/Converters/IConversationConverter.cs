61f6035a97c3db38f8b1658ec8ea3b46993a812d
Bajmóczy Eszter Csilla <36538209+EszterBajmoczy@users.noreply.github.com>
2021-10-29 21:23:47 +0200

    Create converter for conversation

---
+++ Lendship.Backend/Interfaces/Converters/IConversationConverter.cs
@@ -0,0 +1,12 @@
+﻿using Lendship.Backend.DTO;
+using Lendship.Backend.Models;
+
+namespace Lendship.Backend.Interfaces.Converters
+{
+    public interface IConversationConverter
+    {
+        ConversationDto ConvertToDto(Conversation conversation);
+
+        Conversation ConvertToEntity(ConversationDto conversationDto);
+    }
+}
