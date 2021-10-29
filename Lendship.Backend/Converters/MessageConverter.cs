5540f56f0a51fc96de57acbb024998c74f0d5d48
Bajmóczy Eszter Csilla <36538209+EszterBajmoczy@users.noreply.github.com>
2021-10-29 21:46:41 +0200

    Create MessageConverter

---
+++ Lendship.Backend/Converters/MessageConverter.cs
@@ -0,0 +1,43 @@
+﻿using Lendship.Backend.Authentication;
+using Lendship.Backend.DTO;
+using Lendship.Backend.Interfaces.Converters;
+using Lendship.Backend.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Lendship.Backend.Converters
+{
+    public class MessageConverter : IMessageConverter
+    {
+        private IUserConverter _userConverter;
+        public MessageConverter(IUserConverter userConverter)
+        {
+            _userConverter = userConverter;
+        }
+
+        public MessageDto ConvertToDto(Message msg, int conversationId)
+        {
+            //TODO evaluation
+            return new MessageDto()
+            {
+                Id = msg.Id,
+                ConversationId = conversationId,
+                UserFrom = _userConverter.ConvertToDto(msg.UserFrom, 0, 1),
+                Content = msg.Content,
+                Date = msg.Date
+            };
+        }
+
+        public Message ConvertToEntity(MessageDto msgDto, ApplicationUser userFrom)
+        {
+            return new Message()
+            {
+                Id = msgDto.Id ?? 0,
+                UserFrom = userFrom,
+                ConversationId = msgDto.ConversationId,
+                Content = msgDto.Content,
+                Date = msgDto.Date ?? DateTime.Now
+            };
+        }
+    }
+}
---
+++ Lendship.Backend/Interfaces/Converters/IMessageConverter.cs
@@ -0,0 +1,13 @@
+﻿using Lendship.Backend.Authentication;
+using Lendship.Backend.DTO;
+using Lendship.Backend.Models;
+
+namespace Lendship.Backend.Interfaces.Converters
+{
+    public interface IMessageConverter
+    {
+        MessageDto ConvertToDto(Message msg, int conversationId);
+
+        Message ConvertToEntity(MessageDto msgDto, ApplicationUser userFrom);
+    }
+}
