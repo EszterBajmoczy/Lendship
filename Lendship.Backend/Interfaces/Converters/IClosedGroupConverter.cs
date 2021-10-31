d2fe5ab8dcc9e001f954428ca37b42fb39474d0c
Bajmóczy Eszter Csilla <36538209+EszterBajmoczy@users.noreply.github.com>
2021-10-31 21:20:11 +0100

    Create closed group converter

---
+++ Lendship.Backend/Interfaces/Converters/IClosedGroupConverter.cs
@@ -0,0 +1,14 @@
+﻿using Lendship.Backend.Authentication;
+using Lendship.Backend.DTO;
+using Lendship.Backend.Models;
+using System.Collections.Generic;
+
+namespace Lendship.Backend.Interfaces.Converters
+{
+    public interface IClosedGroupConverter
+    {
+        ClosedGroupDto ConvertToDto(ClosedGroup closedGroup, List<ApplicationUser> users);
+
+        ClosedGroup ConvertToEntity(ClosedGroupDto closedGroupDto);
+    }
+}
