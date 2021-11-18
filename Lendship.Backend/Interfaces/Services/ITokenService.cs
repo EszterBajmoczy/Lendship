b0920b74c6dac74f9de7256b607a4807aa4bd668
Bajmóczy Eszter Csilla <eszty.bajmoczy@gmail.com>
2021-11-18 21:02:28 +0100

    Create TokenService

---
+++ Lendship.Backend/Interfaces/Services/ITokenService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Lendship.Backend.Interfaces.Services
+{
+    public interface ITokenService
+    {
+        bool IsCurrentTokenValid();
+
+        bool IsRefreshTokenValid(string refreshToken);
+
+        Task DeactivateCurrentTokenAndRefreshTokenAsync(string refreshToken);
+
+        JwtSecurityToken GenerateNewToken(List<Claim> authClaims, bool isRefresh);
+    }
+}
