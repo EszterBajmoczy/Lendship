7fc1c6bb0bc10a505dad7784ee83b4c69b3a6c45
Bajmóczy Eszter Csilla <eszty.bajmoczy@gmail.com>
2021-11-25 18:38:07 +0100

    Create WeightedAverage and interface

---
+++ Lendship.Backend/EvaluationCalcuting/WeightedAverage.cs
@@ -0,0 +1,23 @@
+﻿using Lendship.Backend.Interfaces.EvaluationCalcuting;
+
+namespace Lendship.Backend.EvaluationCalcuting
+{
+    public class WeightedAverage : IEvaluationCalcuting
+    {
+        public int calculateAdviser(int flexibility, int reliability, int qualityOfProduct)
+        {
+            return (flexibility + reliability * 3 + qualityOfProduct * 3) / 7;
+        }
+
+        public int calculateLender(int flexibility, int reliability, int qualityAtReturn)
+        {
+            return (flexibility + reliability * 3 + qualityAtReturn * 3) / 7;
+        }
+
+        public int recalculate(int currentEvaluation, int currentCount, int evaluation)
+        {
+            var s = currentEvaluation * currentCount;
+            return (s + evaluation) / (currentCount + 1);
+        }
+    }
+}
---
+++ Lendship.Backend/Interfaces/EvaluationCalcuting/IEvaluationCalcuting.cs
@@ -0,0 +1,11 @@
+﻿namespace Lendship.Backend.Interfaces.EvaluationCalcuting
+{
+    public interface IEvaluationCalcuting
+    {
+        int calculateAdviser(int flexibility, int reliability, int qualityOfProduct);
+
+        int calculateLender(int flexibility, int reliability, int qualityAtReturn);
+
+        int recalculate(int currentEvaluation, int currentCount, int evaluation);
+    }
+}
