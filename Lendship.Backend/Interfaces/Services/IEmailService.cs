d95ea2b540245594c96d3d9032c8c223df738d3b
Bajmóczy Eszter Csilla <eszty.bajmoczy@gmail.com>
2022-02-26 16:05:27 +0100

    EmailService

---
+++ Lendship.Backend/Interfaces/Services/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using Lendship.Backend.DTO.Authentication;
+using System.Threading.Tasks;
+
+namespace Lendship.Backend.Interfaces.Services
+{
+    public interface IEmailService
+    {
+        void SendEmailAsync(string username, string email, string url);
+    }
+}
