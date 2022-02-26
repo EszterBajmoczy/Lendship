0eb6b0eb3c98210ffc70b2cd9af622c90bcd657c
Bajmóczy Eszter Csilla <eszty.bajmoczy@gmail.com>
2022-02-26 16:09:24 +0100

    Create ResetPasswordModel.cs

---
+++ Lendship.Backend/DTO/Authentication/ResetPasswordModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lendship.Backend.DTO.Authentication
+{
+    public class ResetPasswordModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "ConfirmPassword is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The two passwords does not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string Token { get; set; }
+    }
+}
