0f2ec431a5381c2fbb474337d5d3250702413c1f
Bajmóczy Eszter Csilla <36538209+EszterBajmoczy@users.noreply.github.com>
2022-04-10 00:01:39 +0200

    Create advertisement model, modify registration user model

--- Lendship.Frontend/src/app/models/registration-user.ts
+++ Lendship.Frontend/src/app/models/registration-user.ts
@@ -1,7 +1,8 @@
 import {LoginUser} from "./login-user";
 
-export class RegisterUser extends LoginUser {
-  name: string | undefined;
-  longitude: string | undefined;
-  latitude: string | undefined;
+export interface RegisterUser extends LoginUser {
+  name: string;
+  longitude: string;
+  latitude: string;
+  location: string;
 }
