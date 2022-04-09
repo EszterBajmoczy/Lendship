2464c599cd6291e845eef3b952426fcdd9e73770
Bajmóczy Eszter Csilla <36538209+EszterBajmoczy@users.noreply.github.com>
2022-04-09 11:57:19 +0200

    Modify username and password validation and update error msg

--- Lendship.Backend/Controllers/AuthenticationController.cs
+++ Lendship.Backend/Controllers/AuthenticationController.cs
@@ -57,7 +57,7 @@ namespace Lendship.Backend.Controllers
             var result = await _userManager.CreateAsync(user, model.Password);
             
             if (!result.Succeeded)
-                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." });
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed! Please check user details and try again. Error: " + result.Errors.FirstOrDefault().Description });
             
             return Ok(new Response { Status = "Success", Message = "User created successfully!" });
         }
