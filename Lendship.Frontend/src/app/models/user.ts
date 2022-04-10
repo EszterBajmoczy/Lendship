9dbc7444ffc26175d5ec06c7b32259239276657a
Bajmóczy Eszter Csilla <36538209+EszterBajmoczy@users.noreply.github.com>
2022-04-10 15:16:34 +0200

    Add models

---
+++ Lendship.Frontend/src/app/models/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+  id: string;
+  name: string;
+  evaluationAsAdvertiser: number;
+  evaluationAsAdvertiserCount: number;
+  evaluationAsLender: number;
+  evaluationAsLenderCount: number;
+}
