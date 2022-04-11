6e1bce04544ed26e8f173b02f852ab4d248a4693
Bajmóczy Eszter Csilla <36538209+EszterBajmoczy@users.noreply.github.com>
2022-04-11 21:31:32 +0200

    ...continue...

--- Lendship.Frontend/src/app/pages/profile-page/profile-page.component.ts
+++ Lendship.Frontend/src/app/pages/profile-page/profile-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { EvaluationAdvertiser, EvaluationLender} from "../../models/evaluations";
+import {ActivatedRoute} from "@angular/router";
+import {UserService} from "../../services/user/user.service";
+import {UserDetail} from "../../models/user-detail";
 
 @Component({
   selector: 'app-profile-page',
@@ -6,8 +10,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-page.component.scss']
 })
 export class ProfilePageComponent implements OnInit {
+  user: UserDetail | undefined;
 
-  constructor() { }
+  evaluationsAdvertiser: EvaluationAdvertiser[] | undefined;
+  evaluationsLender: EvaluationLender[] | undefined;
+
+  showAdvertiserEvaluations: boolean = false;
+  showLenderEvaluations: boolean = false;
+
+  constructor(private userService: UserService, activatedRoute: ActivatedRoute) {
+    activatedRoute.params.subscribe( params => {
+      let id = params['id'];
+      if(id != null){
+        this.userService.getOtherUser(id)
+          .subscribe(user => {
+            console.log("other" +user);
+            this.user = user;
+          });
+      } else {
+        this.userService.getUser()
+          .subscribe(user => {
+            console.log(user);
+            this.user = user;
+            this.loadAdvertiserEvaluations();
+          });
+      }
+    });
+  }
+
+  loadAdvertiserEvaluations(){
+    this.showAdvertiserEvaluations = !this.showAdvertiserEvaluations;
+    if(this.evaluationsAdvertiser === undefined){
+      this.userService.getEvaluationAdvertiserUser(this.user?.id ?? "")
+        .subscribe(evaluations => {
+          console.log(evaluations);
+          this.evaluationsAdvertiser = evaluations;
+        });
+    }
+  }
+
+  loadLenderEvaluations(){
+    this.showLenderEvaluations = !this.showLenderEvaluations;
+    if(this.evaluationsLender === undefined) {
+      console.log(this.user?.id);
+      this.userService.getEvaluationLenderUser(this.user?.id ?? "")
+        .subscribe(evaluations => {
+          console.log(evaluations);
+          this.evaluationsLender = evaluations;
+        });
+    }
+  }
 
   ngOnInit(): void {
   }
--- Lendship.Frontend/src/app/services/user/user.service.ts
+++ Lendship.Frontend/src/app/services/user/user.service.ts
@@ -1,9 +1,72 @@
 import { Injectable } from '@angular/core';
+import { catchError } from "rxjs/operators";
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { map, Observable, throwError } from 'rxjs';
+import { AuthService } from "../auth/auth.service";
+import { UserDetail } from "../../models/user-detail";
+import { GeocodingService} from "../geocoding/geocoding.service";
+import {AdvertisementDetail} from "../../models/advertisement-detail";
+import {EvaluationAdvertiser, EvaluationLender} from "../../models/evaluations";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  headers: HttpHeaders
 
-  constructor() { }
+  constructor(private http: HttpClient, private authService: AuthService, private geoCodingService: GeocodingService) {
+    this.headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.authService.getAccessToken()}`
+    });
+  }
+
+  getUser(): Observable<UserDetail>{
+    return this.http.get<UserDetail>("https://localhost:44377/Profile", { headers: this.headers})
+      .pipe(
+        map((response: UserDetail) => this.setLocation(response)),
+        catchError(this.handleError));
+  }
+
+  getOtherUser(id: string): Observable<UserDetail>{
+    return this.http.get<UserDetail>("https://localhost:44377/Profile/" + id, { headers: this.headers})
+      .pipe(
+        map((response: UserDetail) => this.setLocation(response)),
+        catchError(this.handleError));
+  }
+
+  getEvaluationAdvertiserUser(id: string): Observable<EvaluationAdvertiser[]>{
+    return this.http.get<EvaluationAdvertiser[]>("https://localhost:44377/EvaluationAdvertiser/" + id, { headers: this.headers})
+      .pipe(
+        catchError(this.handleError));
+  }
+
+  getEvaluationLenderUser(id: string): Observable<EvaluationLender[]>{
+    return this.http.get<EvaluationLender[]>("https://localhost:44377/EvaluationLender/" + id, { headers: this.headers})
+      .pipe(
+        catchError(this.handleError));
+  }
+
+  setLocation(user: UserDetail): UserDetail{
+    this.geoCodingService.getAddress(user.latitude, user.longitude)
+      .subscribe(location => {
+        user.location = location["city"]
+      });
+    return user;
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.log("Error auth");
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
 }
