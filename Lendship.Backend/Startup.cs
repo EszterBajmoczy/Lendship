f7a3595dc58ca76df598f4288e80d6b5257dcf2a
Bajmóczy Eszter Csilla <36538209+EszterBajmoczy@users.noreply.github.com>
2022-09-21 01:19:58 +0200

    Update Startup.cs

--- Lendship.Backend/Startup.cs
+++ Lendship.Backend/Startup.cs
@@ -31,6 +31,15 @@ namespace Lendship.Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(
+                    name: "AllowOrigin",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                    });
+            });
 
             services.AddDbContext<LendshipDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
@@ -144,6 +153,8 @@ namespace Lendship.Backend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("AllowOrigin");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
