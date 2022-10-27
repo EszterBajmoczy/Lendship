5346ad951b4e3ae7c3b0ed1f2fd10b779da67384
Bajmóczy Eszter Csilla <36538209+EszterBajmoczy@users.noreply.github.com>
2022-10-27 22:33:30 +0200

    Update IAdvertisementConverter.cs

--- Lendship.Backend/Interfaces/Converters/IAdvertisementConverter.cs
+++ Lendship.Backend/Interfaces/Converters/IAdvertisementConverter.cs
@@ -1,10 +1,5 @@
-﻿using Lendship.Backend.Authentication;
-using Lendship.Backend.DTO;
+﻿using Lendship.Backend.DTO;
 using Lendship.Backend.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Lendship.Backend.Interfaces.Converters
 {
