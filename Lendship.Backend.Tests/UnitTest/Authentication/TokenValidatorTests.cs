52d9ca50f19a2cad6213ec531fdda1dc80b50644
Bajmóczy Eszter Csilla <36538209+EszterBajmoczy@users.noreply.github.com>
2022-10-27 22:39:22 +0200

    Initialize Test project and create tests for TokenValidator

---
+++ Lendship.Backend.Tests/UnitTest/Authentication/TokenValidatorTests.cs
@@ -0,0 +1,100 @@
+using Lendship.Backend.Authentication;
+using Lendship.Backend.Exceptions;
+using Lendship.Backend.Interfaces.Services;
+using Microsoft.IdentityModel.Tokens;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Lendship.Backend.Tests.UnitTest.Authentication
+{
+    [TestFixture]
+    public class TokenValidatorTests
+    {
+        private TokenValidator _sut;
+        private Mock<ITokenService> _tokenService;
+        private Mock<JwtSecurityTokenHandler> _tokenHandler;
+
+        [SetUp]
+        public void Setup()
+        {
+            _tokenService = new Mock<ITokenService>();
+            _tokenHandler = new Mock<JwtSecurityTokenHandler>();
+            _sut = new TokenValidator(_tokenService.Object, _tokenHandler.Object);
+        }
+
+        [TestCase]
+        public void CanValidateToken_Should_Return_True()
+        {
+            var result = _sut.CanValidateToken;
+            Assert.IsTrue(result);
+        }
+
+        [TestCase]
+        public void ValidateToken_Should_Throw_Error_When_Token_Is_Deactivated()
+        {
+            _tokenService.Setup(x => x.IsCurrentTokenDeactivated()).Returns(true);
+            try
+            {
+                var result = _sut.ValidateToken("", null, out SecurityToken token);
+                Assert.Fail();
+            } catch (InvalidTokenException)
+            {
+                Assert.Pass();
+            }
+        }
+
+
+        [TestCase]
+        public void ValidateToken_Should_Call_JWTTokenHandler_ValidateToken_If_Token_Is_Not_Deactivated()
+        {
+            _tokenService.Setup(x => x.IsCurrentTokenDeactivated()).Returns(false);
+
+            SecurityToken token = null;
+            var result = _sut.ValidateToken("", null, out token);
+
+            _tokenHandler.Verify(x => x.ValidateToken(It.IsAny<string>(), It.IsAny<TokenValidationParameters>(), out token), Times.Once);
+        }
+
+
+        [TestCase]
+        public void ValidateToken_Should_Not_Throw_Error_When_Token_Is_Valid()
+        {
+            try
+            {
+                SecurityToken token = null;
+
+                _tokenService.Setup(x => x.IsCurrentTokenDeactivated()).Returns(false);
+                _tokenHandler.Setup(x => x.ValidateToken(It.IsAny<string>(), It.IsAny<TokenValidationParameters>(), out token)).Returns(It.IsAny<ClaimsPrincipal>());
+
+                var result = _sut.ValidateToken("", null, out token);
+                Assert.Pass();
+            } catch (InvalidTokenException)
+            {
+                Assert.Fail();
+            }
+        }
+
+
+        [TestCase]
+        public void ValidateToken_Should_Not_Throw_Error_When_Token_Is_Not_Valid()
+        {
+            try
+            {
+                SecurityToken token = null;
+
+                _tokenService.Setup(x => x.IsCurrentTokenDeactivated()).Returns(false);
+                _tokenHandler.Setup(x => x.ValidateToken(It.IsAny<string>(), It.IsAny<TokenValidationParameters>(), out token)).Throws(It.IsAny<InvalidTokenException>());
+
+                var result = _sut.ValidateToken("", null, out token);
+                Assert.Fail();
+            }
+            catch (InvalidTokenException)
+            {
+                Assert.Pass();
+            }
+        }
+    }
+}
---
+++ Lendship.Backend.Tests/UnitTest/Services/AdvertisementServiceTests.cs
@@ -0,0 +1,62 @@
+﻿using Lendship.Backend.Interfaces.Converters;
+using Lendship.Backend.Interfaces.Services;
+using Lendship.Backend.Models;
+using Lendship.Backend.Services;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lendship.Backend.Tests.UnitTest.Services
+{
+    [TestFixture]
+    internal class AdvertisementServiceTests
+    {
+        private IAdvertisementService _sut;
+        private Mock<IHttpContextAccessor> _contextAccessor;
+        private Mock<LendshipDbContext> _dbContext;
+        private Mock<ICategoryService> _categoryService;
+        private Mock<INotificationService> _notificationService;
+        private Mock<IImageService> _imageService;
+
+        private Mock<IAdvertisementDetailsConverter> _advertisementDetailsConverter;
+        private Mock<IAdvertisementConverter> _advertisementConverter;
+        private Mock<IAvailabilityConverter> _availabilityConverter;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _contextAccessor = new Mock<IHttpContextAccessor>();
+            _dbContext = new Mock<LendshipDbContext>();
+            _categoryService = new Mock<ICategoryService>();
+            _notificationService = new Mock<INotificationService>();
+            _imageService = new Mock<IImageService>();
+
+            _advertisementDetailsConverter = new Mock<IAdvertisementDetailsConverter>();
+            _advertisementConverter = new Mock<IAdvertisementConverter>();
+            _availabilityConverter = new Mock<IAvailabilityConverter>();
+            /*
+            _sut = new AdvertisementService(
+                _contextAccessor.Object, 
+                _dbContext.Object, 
+                _categoryService.Object, 
+                _notificationService.Object,
+                _imageService.Object,
+                _advertisementDetailsConverter.Object,
+                _advertisementConverter.Object,
+                _availabilityConverter.Object);*/
+        }
+
+        [TestCase]
+        public void GetAdvertisement()
+        {
+            var s = new List<Advertisement>() { new Advertisement() };
+            //_dbContext.Setup(x => x.Advertisements).Returns(s);
+        }
+    }
+}
