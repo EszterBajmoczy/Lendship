8cb355344eb392319c12d089faa575762bf49bf7
Bajmóczy Eszter Csilla <36538209+EszterBajmoczy@users.noreply.github.com>
2022-11-20 22:13:16 +0100

    Fix cli

--- Lendship.Frontend/src/environments/environment.prod.ts
+++ Lendship.Frontend/src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   baseUrl: "http://0.0.0.0:5000/",
   geocodingBaseUrl: "https://geocode.xyz/",
-  geocodingApiKey: "318774527394438479612x29999"
+  geocodingApiKey: "318774527394438479612x29999",
+  baseImage: "//blank.jpg",
 };
