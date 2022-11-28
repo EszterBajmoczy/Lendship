6c46f5820416e104b67c49453a999cd50c1cd261
Bajmóczy Eszter Csilla <36538209+EszterBajmoczy@users.noreply.github.com>
2022-11-28 22:17:38 +0100

    Modify date format

--- Lendship.Frontend/src/app/services/date-handler/date-handler.service.ts
+++ Lendship.Frontend/src/app/services/date-handler/date-handler.service.ts
@@ -10,11 +10,11 @@ export class DateHandlerService {
 
   convertDateToString(date: Date): string {
     date.setDate(date.getDate() + 1);
-    let result = `${date.getFullYear()}-`;
+    let result = `${date.getFullYear()}.`;
     if((date.getUTCMonth() + 1) < 10) {
-      result += `0${(date.getUTCMonth() + 1)}-`;
+      result += `0${(date.getUTCMonth() + 1)}.`;
     } else {
-      result += `${(date.getUTCMonth() + 1)}-`;
+      result += `${(date.getUTCMonth() + 1)}.`;
     }
     if(date.getUTCDate() < 10){
       result += `0${date.getUTCDate()}`;
@@ -40,11 +40,11 @@ export class DateHandlerService {
     d.setMonth(date.month-1);
     d.setDate(date.day);
 
-    let result = `${d.getFullYear()}-`;
+    let result = `${d.getFullYear()}.`;
     if((d.getUTCMonth() + 1) < 10) {
-      result += `0${(d.getUTCMonth() + 1)}-`;
+      result += `0${(d.getUTCMonth() + 1)}.`;
     } else {
-      result += `${(d.getUTCMonth() + 1)}-`;
+      result += `${(d.getUTCMonth() + 1)}.`;
     }
     if(d.getUTCDate() < 10){
       result += `0${d.getUTCDate()}`;
