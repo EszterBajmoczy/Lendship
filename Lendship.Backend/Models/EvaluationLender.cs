200bc43324333889e15062ac484afeffddf64143
Bajmóczy Eszter Csilla <36538209+EszterBajmoczy@users.noreply.github.com>
2022-11-29 12:23:22 +0100

    Refact evaluation models

--- Lendship.Backend/Models/EvaluationLender.cs
+++ Lendship.Backend/Models/EvaluationLender.cs
@@ -1,42 +1,12 @@
 ﻿using Lendship.Backend.Authentication;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Lendship.Backend.Models
 {
-    public class EvaluationLender
+    public class EvaluationLender : Evaluation
     {
-        [Key]
-        public int Id { get; set; }
-
-        [Required]
-        public ApplicationUser UserFrom { get; set; }
-
-        [Required]
-        public ApplicationUser UserTo { get; set; }
-
-        [Required]
-        public Advertisement Advertisement { get; set; }
-
-        [Required]
-        public int Flexibility { get; set; }
-
-        [Required]
-        public int Reliability { get; set; }
-
         [Required]
         public int QualityAtReturn { get; set; }
-
-        public string Comment { get; set; }
-
-        [Required]
-        public bool IsAnonymous { get; set; }
-
-        [Required]
-        public DateTime Creation { get; set; }
     }
 }
